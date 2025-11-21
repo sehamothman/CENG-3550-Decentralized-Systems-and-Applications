5958b3b688cdb09248ad952a1c0f2ba3f3cc62d0
Seham Othman <210709718@posta.mu.edu.tr>
2025-11-21 21:13:47 +0300

    Update 3_Arrays.sol

--- SolidityBasics/3_Arrays.sol
+++ SolidityBasics/3_Arrays.sol
@@ -1,17 +1,24 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.30;
 
-contract Arrays {
-    uint256[] public uint256Array = [1, 2, 3];
-    string[] public stringArray = ["apple", "banana", "carrot"];
-    string[] public values;
-    uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
+    contract Contract2 {
+        //ARRAYS : are sorted groupings of different pieces of data.
+        uint [] public uintarray = [1,2,3];
+        uint256[] public uint256Array = [1, 2, 3];
+        string[] public stringArray = ["apple", "banana", "carrot"];
+        //i can create empty array !
+        string[] public values; //State variable
+        // creating 2D arrays!
+        uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
 
-    function addValue(string memory _value) public {
-        values.push(_value);
+
+        function addValue(string memory _value) public {
+            //_value variable is local variable! 
+            values.push(_value);
+        }
+        //get the length of an array!
+        function valueCount() public view returns (uint256) {
+            return values.length; //length is property not function!
+        }    
+
     }
-
-    function valueCount() public view returns (uint256) {
-        return values.length;
-    }
-}
