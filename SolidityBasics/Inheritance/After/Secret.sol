b8257f7cd1c70a190b0bb51d717bd1959e62aed4
Seham Othman <sehamhakim262004@gmail.com>
2025-11-23 14:20:21 +0300

    Update Secret.sol

--- SolidityBasics/Inheritance/After/Secret.sol
+++ SolidityBasics/Inheritance/After/Secret.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// Showcase contract inheritance
+pragma solidity ^0.8.0; //telling the compiler what version we will use!
+//1- Inheritence :  A child contract can inherit some behaviours from its parent contract.
+//2- Fcatories :smart contract that creates other smart contracts!
+//3- Interaction : call smart contracts inside another smart contracts!
 
 contract Ownable {
     address owner;
 
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender; //The address of person who deploy the contract
     }
 
-    modifier onlyOwner() {
+    modifier onlyOwner() { // make sure the owner is the one who deploy the contract and requests the secret
         require(msg.sender == owner, "must be owner");
         _;
     }
@@ -28,12 +29,13 @@ contract SecretVault {
     }
 }
 
-contract Secret is Ownable {
+//Factory: smart contract that creates other smart contracts!
+contract Secret is Ownable { //will fetch the secret from the SecretVault contract
     address secretVault;
 
     constructor(string memory _secret) {
-        SecretVault _secretVault = new SecretVault(_secret);
-        secretVault = address(_secretVault);
+        SecretVault _secretVault = new SecretVault(_secret); //create instance of a contract! new contract created!
+        secretVault = address(_secretVault); //save address of secret vault!
         super;
     }
 
