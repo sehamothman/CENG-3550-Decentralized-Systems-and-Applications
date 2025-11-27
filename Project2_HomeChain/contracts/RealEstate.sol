216e20179275bc68c272333869ed3fbe8479ded6
Seham Othman <210709718@posta.mu.edu.tr>
2025-11-27 22:24:43 +0300

    real estate contract is written & started with escrow and made a small test for it in Test folder

--- Project2_HomeChain/contracts/RealEstate.sol
+++ Project2_HomeChain/contracts/RealEstate.sol
@@ -7,15 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract RealEstate is ERC721URIStorage {
     // 1- Use Third party library to help creating the NFT Quickly!
-    //Zeppelin library is a great resource https://docs.openzeppelin.com/contracts/5.x/erc721
-    //zeppelin was installed with npm ! 
+    //OpenZeppelin library is a great resource https://docs.openzeppelin.com/contracts/5.x/erc721
+    //Openzeppelin was installed with npm ! 
 
-    using Counters for Counters.Counter;
+    //ERC-721 is basically a type of contract that lets you create these unique tokens, and each token is kind of like its own individual entity with its own metadata. So you can think of each token as a unique digital item that stands on its own and represents something one-of-a-kind.
+
+    using Counters for Counters.Counter; //use all the helpful functions that the Counters library provides for this Counter type.
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("Real Estate", "REAL") {}
 
     function mint(string memory tokenURI) public returns (uint256) {
+        //after the contract created and deployed to the blockchain we will call the MINT function it will create this properties from scratch
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -26,7 +29,7 @@ contract RealEstate is ERC721URIStorage {
     }
 
     function totalSupply() public view returns (uint256) {
-        return _tokenIds.current();
+        return _tokenIds.current(); //represents how many tokens have been minted so far.
     }
 
 
--- Project2_HomeChain/test/Escrow.js
+++ Project2_HomeChain/test/Escrow.js
@@ -1,3 +1,5 @@
+//In test we can write JavaScript code to test the behavior of a smart contract if it works well!
+
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
@@ -5,6 +7,15 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+// ALL TEST CASES FOR ESCROW SMART CONTRACT
 describe('Escrow', () => {
+    it('saves the address', async () => {
+        // Deploy Real Estate
+        const RealEstate = await ethers.getContractFactory('RealEstate')
+        realEstate = await RealEstate.deploy()
 
-})
+        console.log("Real Estate Contract Address:", realEstate.address)
+
+    })
+    
+    })
